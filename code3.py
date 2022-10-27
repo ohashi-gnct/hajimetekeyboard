438784dc5c8426432e6f80d631592fe181c076c8
ohashi-gnct <84501054+ohashi-gnct@users.noreply.github.com>
2022-10-27 20:52:41 +0900

    code3.py CdSセルに応じて高さが変わる

--- code3.py
+++ code3.py
@@ -1,11 +1,13 @@
+# ここにコードを書いてね :-)
 import audiocore
 import audiopwmio
 import board
 import array
 import time
 import math
+import analogio
 
-# 1周期のサイン波を作ってサンプル配列とする
+# 1周期の方形波を作ってサンプル配列とする
 def generate_sine_sample(freq):
     sr = 44100
     length = sr // freq
@@ -13,14 +15,30 @@ def generate_sine_sample(freq):
     for i in range(length):
         if i < (length // 2):
             sine_wave[i] = 2 ** 15 - 1
-    return(audiocore.RawSample(sine_wave, sample_rate=sr))
+    return audiocore.RawSample(sine_wave, sample_rate=sr)
+
 
 dac = audiopwmio.PWMAudioOut(board.GP22)
 tones_freq = [523, 587, 659, 698, 784, 880, 988, 1047]
 
+# 音階のしきい値 近いほど高い音
+adc_thresholds = [13000, 13300, 13800, 14600, 15500, 18000, 38000, 65535]
+
+# 無音になるしきい値
+adc_threshold_stop = 12000
+adc = analogio.AnalogIn(board.A1)
+
 while True:
-    for freq in tones_freq:
-        dac.play(generate_sine_sample(freq), loop=True)
-        time.sleep(0.5)
+    for freq, threshold in zip(tones_freq, adc_thresholds):
+        if adc.value < adc_threshold_stop:
+            dac.stop()
+            time.sleep(0.1)
+            break
         
-        
+        elif adc.value < threshold:
+            dac.play(generate_sine_sample(freq), loop=True)
+            time.sleep(0.1)
+            break
+    
+    
+
